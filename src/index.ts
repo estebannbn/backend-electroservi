3274224d6ccad351673dd87dafe4ba4fb13b42e2
Esteban <estebannahuel2010@live.com>
2025-09-19 14:47:12 -0300

    ts-node-dev cambiado por tsx

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import router from "./routes/app.routes";
+import appRoutes from "./routes/app.routes";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use('/api',router)
+app.use('/api',appRoutes)
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
